a5c4948a262686f1c198a9187b8548aa9330ec0a
AnandGummadilli <anandg99@yahoo.com>
2025-08-04 14:44:51 +0530

    added more test cases

--- tests2/APITests.spec.ts
+++ tests2/APITests.spec.ts
@@ -34,5 +34,24 @@ test('Get Pet By NOT Available PetID', async ({ request }) => {
     console.log("response body:" + await response.body().toString());
     expect(await response.statusText()).toBe("Not Found");
     expect(await response.status()).toBe(404);
-        
+    expect(await response.json()).toHaveProperty('message','Pet not found');
+    expect(await response.json()).toHaveProperty('code', 1);
+    //expect(await response.json()).toMatch('error');
+    
+});
+//Delete non existing PET
+test('Delete non existing Pet', async ({ request }) => {
+  const response = await request.delete('https://petstore.swagger.io/v2/pet/786');
+    expect(await response.statusText()).toBe("Not Found");
+    expect(await response.status()).toBe(404);
+    
+});
+
+//Delete existing PET
+test('Delete existing Pet', async ({ request }) => {
+  //precondition: petID: 12345669 exists
+  const response = await request.delete('https://petstore.swagger.io/v2/pet/12345669');
+    //expect(await response.statusText()).toBe("");
+    expect(await response.status()).toBe(200);
+    
 });
